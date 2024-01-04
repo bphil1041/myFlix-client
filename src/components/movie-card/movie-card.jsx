e6e50ade18d1b558847504b80cec7347ec1be356
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-01-04 08:20:22 -0600

    define prop constraints for Movie Card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -12,3 +12,23 @@ export const MovieCard = ({ movie, onMovieClick }) => {
     );
 };
 
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        genre: PropTypes.shape({
+            genreName: PropTypes.string,
+            description: PropTypes.string,
+        }),
+        director: PropTypes.shape({
+            name: PropTypes.string,
+            birth: PropTypes.string,
+            death: PropTypes.string,
+            bio: PropTypes.string,
+        }),
+        _id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        year: PropTypes.string,
+        description: PropTypes.string,
+        MovieId: PropTypes.string,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired,
+};
