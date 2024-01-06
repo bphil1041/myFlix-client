861e73e3b9fe306bd749e679ebd24ab12499de26
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-01-05 18:52:26 -0600

    show year as PropTypes.number

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
@@ -14,13 +17,35 @@ export const MovieView = ({ movie, onBackClick }) => {
             </div>
             <div>
                 <span>Genre: </span>
-                <span>{movie.genre}</span>
+                <span>{movie.genre.genreName}</span>
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movie.director}</span>
+                <span>{movie.director.name}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
     );
+};
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        image: PropTypes.string,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        genre: PropTypes.shape({
+            genreName: PropTypes.string,
+            description: PropTypes.string,
+        }),
+        director: PropTypes.shape({
+            name: PropTypes.string,
+            birth: PropTypes.string,
+            death: PropTypes.string,
+            bio: PropTypes.string,
+        }),
+        _id: PropTypes.string.isRequired,
+        MovieId: PropTypes.string,
+        year: PropTypes.number,
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired,
 };
