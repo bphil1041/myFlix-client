61fde248f3c0bd4dadf7c41f0d36a5c773a3e18d
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-01-07 11:12:29 -0600

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -16,12 +16,32 @@ export const MovieView = ({ movie, onBackClick }) => {
                 <span>{movie.description}</span>
             </div>
             <div>
+                <span>Release Year: </span>
+                <span>{movie.year}</span>
+            </div>
+            <div>
                 <span>Genre: </span>
                 <span>{movie.genre.genreName}</span>
             </div>
             <div>
+                <span>Genre Description: </span>
+                <span>{movie.genre.description}</span>
+            </div>
+            <div>
                 <span>Director: </span>
                 <span>{movie.director.name}</span>
+            </div>
+            <div>
+                <span>Born: </span>
+                <span>{movie.director.birth}</span>
+            </div>
+            <div>
+                <span>Died: </span>
+                <span>{movie.director.death}</span>
+            </div>
+            <div>
+                <span>Bio: </span>
+                <span>{movie.director.bio}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
