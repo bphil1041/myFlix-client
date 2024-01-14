38bf8a3bdce8b19173200539b27dc18e72e3b268
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-01-14 16:11:44 -0600

    create cards

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,20 @@
 import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <div
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
-            {movie.title}
-        </div>
+        <Card className="h-100">
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Button onClick={() => {
+                    onMovieClick(movie);
+                }} variant="link">
+                    Open
+                </Button>
+
+                <Card.Title>{movie.title}</Card.Title>
+            </Card.Body>
+        </Card>
     );
 };
 
