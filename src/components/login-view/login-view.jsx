4b62c8f9e3bb5df170c50415c70c9edd80df8d88
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-01 08:03:27 -0600

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -13,7 +13,7 @@ export const LoginView = ({ onLoggedIn }) => {
             secret: password,
         };
 
-        fetch("https://myflixbp-ee7590ef397f.herokuapp.com/login", {
+        fetch("https://myflixbp-ee7590ef397f.herokuapp.com", {
             method: "POST",
             body: JSON.stringify(data),
         }).then((response) => {
