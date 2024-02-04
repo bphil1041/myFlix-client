0f606f7b1a6a85f78b487318a0520da5c93a2d04
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-04 17:13:36 -0600

    Update profile-view.jsx

--- src/profile-view/profile-view.jsx
+++ src/profile-view/profile-view.jsx
@@ -38,8 +38,8 @@ export const ProfileView = ({ user, movies, setUser, removeFav, addFav }) => {
                 method: "PUT",
                 body: JSON.stringify(data),
                 headers: {
+                    Authorization: `Bearer ${token}`,
                     "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
                 }
             });
 
@@ -68,8 +68,9 @@ export const ProfileView = ({ user, movies, setUser, removeFav, addFav }) => {
         fetch(`https://myflixbp-ee7590ef397f.herokuapp.com/users/${user.name}`, {
             method: "DELETE",
             headers: {
-                Authorization: `Bearer ${token}`
-            }
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
         }).then((response) => {
             if (response.ok) {
                 setUser(null);
