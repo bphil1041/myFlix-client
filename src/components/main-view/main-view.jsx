f5825a82af8dadb335262e3181a48fc2f35f7f89
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-04 15:30:41 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -140,7 +140,7 @@ export const MainView = () => {
                                         {movies.map((movie) => (
                                             <Col className="mb-5" key={movie._id} md={3}>
                                                 <Link to={`/movies/${movie._id}`}>
-                                                    <MovieCard movie={movie} />
+                                                    <MovieCard className="font" movie={movie} />
                                                 </Link>
                                             </Col>
                                         ))}
