a8217744d94568b682ee1057371bd82f9c7d2f5d
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-05 16:41:02 -0600

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -43,7 +43,7 @@ export const ProfileView = ({ user, movies, setUser }) => {
         const fetchUserData = async () => {
             try {
                 // Your Heroku backend API URL
-                const apiUrl = `https://myflixbp-ee7590ef397f.herokuapp.com/users/${user.name}`;
+                const apiUrl = `https://myflixbp-ee7590ef397f.herokuapp.com/users/${Username}`;
 
                 const response = await fetch(apiUrl, {
                     method: "GET",
