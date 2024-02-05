1ce94d1ffb469360258e26a3261fa6bcd4823927
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-05 14:44:56 -0600

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./navigation-bar.scss";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
     return (
         <Navbar bg="primary" expand="lg" fixed="top">
             <Container>
-                <Navbar.Brand as={Link} to="/">
-                    MyFlix
+                <Navbar.Brand className="nav-title" as={Link} to="/">
+                    myFlix
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         {!user ? (
                             <>
-                                <Nav.Link as={Link} to="/login">
+                                <Nav.Link className="navlink" as={Link} to="/login">
                                     Login
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/signup">
+                                <Nav.Link className="navlink" as={Link} to="/signup">
                                     Signup
                                 </Nav.Link>
                             </>
                         ) : (
                             <>
-                                <Nav.Link as={Link} to="/">
+                                <Nav.Link className="navlink" as={Link} to="/">
                                     Home
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/profile">
+                                <Nav.Link className="navlink" as={Link} to="/profile">
                                     Profile
                                 </Nav.Link>
-                                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                                <Nav.Link className="navlink" onClick={onLoggedOut}>Logout</Nav.Link>
                             </>
                         )}
                     </Nav>
