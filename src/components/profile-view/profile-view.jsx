7fdbbc4c83b9a4acce5d169586874b3ea477774b
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-05 15:15:48 -0600

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -172,10 +172,12 @@ export const ProfileView = ({ user, movies, setUser }) => {
                             key={movie._id}
                             className="m-3"
                         >
-                            <MovieCard
-                                movie={movie}
-                                onMovieClick={() => handleMovieCardClick(movie)}
-                            />
+                            <Card className="h-100">
+                                <Card.Body>
+                                    <Card.Title className="card-title">{movie.title}</Card.Title>
+                                    <Button onClick={() => onMovieClick(movie)} variant="link"></Button>
+                                </Card.Body>
+                            </Card>
                         </Col>
                     ))
                 ) : (
