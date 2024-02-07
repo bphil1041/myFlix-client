a41ccc4966f76c61e0ba53337b6a94cd760d8401
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-06 19:54:25 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -156,7 +156,7 @@ export const MainView = () => {
                                         {movies.map((movie) => (
                                             <Col className="mb-5" key={movie._id} md={3}>
                                                 <Link to={`/movies/${movie._id}`}>
-                                                    <MovieCard className="card"
+                                                    <MovieCard
                                                         movie={movie}
                                                         image={movie.image} // Pass image prop to MovieCard
                                                     />
