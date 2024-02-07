20123547426e69a28dd9eb3bae3899cac3c531c8
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-06 18:27:49 -0600

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,7 +7,7 @@ export const MovieCard = ({ movie, onMovieClick, image }) => {
             <Card.Img variant="top" src={image} alt={movie.title} />
             <Card.Body>
                 <Card.Title className="card-title">{movie.title}</Card.Title>
-                <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                <Button onClick={() => onMovieClick(movie)} variant="link"></Button>
             </Card.Body>
         </Card>
     );
