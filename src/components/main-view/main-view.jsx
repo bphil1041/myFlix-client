bb8f0e522354f2e1a778ed0d6feb4056af1b893e
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-07 09:10:16 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -106,6 +106,17 @@ export const MainView = () => {
                 }}
             />
 
+            <Row className="justify-content-md-center">
+                <Col md={3}>
+                    <select className="form-control" onChange={handleGenreChange}>
+                        <option value="">All Genres</option>
+                        {movies.map(movie => (
+                            <option key={movie._id} value={movie.genre.genreName}>{movie.genre.genreName}</option>
+                        ))}
+                    </select>
+                </Col>
+            </Row>
+
 
             <Row className="justify-content-md-center">
                 <Routes>
@@ -178,21 +189,6 @@ export const MainView = () => {
 
                 </Routes>
             </Row>
-
-            <Row className="justify-content-md-center">
-                <Col md={3}>
-                    <select className="form-control" onChange={handleGenreChange}>
-                        <option value="">All Genres</option>
-                        {movies.map(movie => (
-                            <option key={movie._id} value={movie.genre.genreName}>{movie.genre.genreName}</option>
-                        ))}
-                    </select>
-                </Col>
-            </Row>
-
-
-
-
         </BrowserRouter>
     );
 };
