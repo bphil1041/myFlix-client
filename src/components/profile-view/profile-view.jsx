62480f78ad3715b34f05829cdd7bf9c3bdceff39
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-06 20:59:28 -0600

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -145,7 +145,7 @@ export const ProfileView = ({ user, setUser, movies }) => {
             if (response.ok) {
                 const updatedUserData = await response.json();
                 console.log("Updated user data:", updatedUserData);
-                setUser(updatedUserData);
+                setUpdatedUser(updatedUserData); // Update updatedUser state
                 alert("Movie added to favorites successfully");
             } else {
                 alert("Failed to add movie to favorites");
@@ -154,6 +154,7 @@ export const ProfileView = ({ user, setUser, movies }) => {
             console.error("Add movie to favorites error:", error);
         }
     };
+
 
 
 
