c7b9a9355c9e572202fb9d88032a60234f47690e
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-07 08:35:30 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -179,7 +179,16 @@ export const MainView = () => {
                 </Routes>
             </Row>
 
-
+            <Row className="justify-content-md-center">
+                <Col md={3}>
+                    <select className="form-control" onChange={handleGenreChange}>
+                        <option value="">All Genres</option>
+                        {movies.map(movie => (
+                            <option key={movie._id} value={movie.genre.genreName}>{movie.genre.genreName}</option>
+                        ))}
+                    </select>
+                </Col>
+            </Row>
             <Row className="justify-content-md-center">
                 {filteredMovies.length > 0 ? (
                     filteredMovies.map(movie => (
