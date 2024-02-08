01261c46b29cecb4bc56c5ea9aa9031ccb775166
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-08 08:25:33 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -21,12 +21,6 @@ export const MainView = () => {
         favoriteMovies: [],
     });
 
-    const [showFilters, setShowFilters] = useState(true); // Initially show filters
-
-    const toggleFilters = () => {
-        setShowFilters(!showFilters);
-    };
-
 
 
     useEffect(() => {
@@ -141,39 +135,39 @@ export const MainView = () => {
                 }}
             />
 
-            {showFilters && (
-                <Row className="justify-content-md-center">
-                    <>
-                        <Col md={3}>
-                            <Dropdown className="genre-filter">
-                                <Dropdown.Toggle variant="primary" id="genre-filter-dropdown">
-                                    {selectedGenre ? selectedGenre : "All Genres"}
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item onClick={() => handleGenreChange("")}>All Genres</Dropdown.Item>
-                                    {selectedGenres.map(genre => (
-                                        <Dropdown.Item key={genre} onClick={() => handleGenreChange(genre)}>{genre}</Dropdown.Item>
-                                    ))}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Col>
-                        <Col md={3}>
-                            <Dropdown className="director-filter">
-                                <Dropdown.Toggle variant="primary" id="director-filter-dropdown">
-                                    {selectedDirector ? selectedDirector : "All Directors"}
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item onClick={() => handleDirectorChange("")}>All Directors</Dropdown.Item>
-                                    {selectedDirectors.map(director => (
-                                        <Dropdown.Item key={director} onClick={() => handleDirectorChange(director)}>{director}</Dropdown.Item>
-                                    ))}
-                                </Dropdown.Menu>
 
-                            </Dropdown>
-                        </Col>
-                    </>
-                </Row>
-            )}
+            <Row className="justify-content-md-center">
+                <>
+                    <Col md={3}>
+                        <Dropdown className="genre-filter">
+                            <Dropdown.Toggle variant="primary" id="genre-filter-dropdown">
+                                {selectedGenre ? selectedGenre : "All Genres"}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => handleGenreChange("")}>All Genres</Dropdown.Item>
+                                {selectedGenres.map(genre => (
+                                    <Dropdown.Item key={genre} onClick={() => handleGenreChange(genre)}>{genre}</Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                    <Col md={3}>
+                        <Dropdown className="director-filter">
+                            <Dropdown.Toggle variant="primary" id="director-filter-dropdown">
+                                {selectedDirector ? selectedDirector : "All Directors"}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => handleDirectorChange("")}>All Directors</Dropdown.Item>
+                                {selectedDirectors.map(director => (
+                                    <Dropdown.Item key={director} onClick={() => handleDirectorChange(director)}>{director}</Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+
+                        </Dropdown>
+                    </Col>
+                </>
+            </Row>
+
 
             <Row className="justify-content-md-center">
                 <Routes>
