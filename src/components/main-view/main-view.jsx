5295657bce09e68630b899114e9162590c6ccf54
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-09 09:06:08 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -141,35 +141,34 @@ export const MainView = () => {
 
 
             <Row className="justify-content-md-center">
-                {location.pathname === "/" && user && movies.length > 0 && (
-                    <>
-                        <Col md={3}>
-                            <Dropdown className="genre-filter">
-                                <Dropdown.Toggle variant="primary" id="genre-filter-dropdown">
-                                    {selectedGenre ? selectedGenre : "All Genres"}
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item onClick={() => handleGenreChange("")}>All Genres</Dropdown.Item>
-                                    {selectedGenres.map(genre => (
-                                        <Dropdown.Item key={genre} onClick={() => handleGenreChange(genre)}>{genre}</Dropdown.Item>
-                                    ))}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Col>
-                        <Col md={3}>
-                            <Dropdown className="director-filter">
-                                <Dropdown.Toggle variant="primary" id="director-filter-dropdown">
-                                    {selectedDirector ? selectedDirector : "All Directors"}
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item onClick={() => handleDirectorChange("")}>All Directors</Dropdown.Item>
-                                    {selectedDirectors.map(director => (
-                                        <Dropdown.Item key={director} onClick={() => handleDirectorChange(director)}>{director}</Dropdown.Item>
-                                    ))}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Col>
-                    </>
+                {location.pathname === "/" && user && movies.length > 0 && (<>
+                    <Col md={3}>
+                        <Dropdown className="genre-filter">
+                            <Dropdown.Toggle variant="primary" id="genre-filter-dropdown">
+                                {selectedGenre ? selectedGenre : "All Genres"}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => handleGenreChange("")}>All Genres</Dropdown.Item>
+                                {selectedGenres.map(genre => (
+                                    <Dropdown.Item key={genre} onClick={() => handleGenreChange(genre)}>{genre}</Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                    <Col md={3}>
+                        <Dropdown className="director-filter">
+                            <Dropdown.Toggle variant="primary" id="director-filter-dropdown">
+                                {selectedDirector ? selectedDirector : "All Directors"}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => handleDirectorChange("")}>All Directors</Dropdown.Item>
+                                {selectedDirectors.map(director => (
+                                    <Dropdown.Item key={director} onClick={() => handleDirectorChange(director)}>{director}</Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                </>
                 )}
             </Row>
 
