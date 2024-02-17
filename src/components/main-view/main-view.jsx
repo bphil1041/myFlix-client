f6ea3b4f694deb2dc4bc2938df04b2d012983bdb
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-16 23:35:09 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -117,28 +117,44 @@ export const MainView = () => {
             />
 
             <Row className="justify-content-md-center">
-                {!user ? (
-                    <Col md={5}>
-                        <LoginView onLoggedIn={(user) => setUser(user)} />
-                    </Col>
-                ) : (
-                    <Routes>
-                        <Route
-                            path="/signup"
-                            element={<SignupView />}
-                        />
-                        <Route
-                            path="/movies/:movieId"
-                            element={<MovieView movies={movies} />}
-                        />
-                        <Route
-                            path="/profile"
-                            element={<ProfileView user={user} movies={movies} setUser={setUser} />}
-                        />
-                        <Route
-                            path="/"
-                            element={
-                                movies.length === 0 ? (
+                <Routes>
+                    <Route
+                        path="/login"
+                        element={
+                            <>
+                                {user ? (
+                                    <Navigate to="/" />
+                                ) : (
+                                    <Col md={5}>
+                                        <LoginView onLoggedIn={(user) => setUser(user)} />
+                                    </Col>
+                                )}
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/movies/:movieId"
+                        element={<MovieView movies={movies} />}
+                    />
+                    <Route
+                        path="/profile"
+                        element={
+                            <>
+                                {user ? (
+                                    <ProfileView user={user} movies={movies} setUser={setUser} />
+                                ) : (
+                                    <Navigate to="/login" replace />
+                                )}
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                {!user ? (
+                                    <Navigate to="/login" replace />
+                                ) : movies.length === 0 ? (
                                     <Col>The list is empty!</Col>
                                 ) : (
                                     <>
@@ -181,12 +197,21 @@ export const MainView = () => {
                                             ))}
                                         </Row>
                                     </>
-                                )
-                            }
-                        />
-                    </Routes>
-                )}
+                                )}
+                            </>
+                        }
+                    />
+                </Routes>
             </Row>
+
+            <Route
+                path="/signup"
+                element={
+                    <Col md={5}>
+                        <SignupView />
+                    </Col>
+                }
+            />
         </BrowserRouter>
     );
 
